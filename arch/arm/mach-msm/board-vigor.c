c0a7389c60cd3b19307954ca060b8ed8d043a266
Dennis <dsb9938@yahoo.com>
2012-09-03 13:50:05 -0400

    Added USB OTG and USB Headphone support.

--- arch/arm/mach-msm/board-vigor.c
+++ arch/arm/mach-msm/board-vigor.c
@@ -1418,6 +1418,7 @@ static int vigor_phy_init_seq[] = { 0x06, 0x36, 0x0C, 0x31, 0x31, 0x32, 0x1, 0x0
 static struct msm_otg_platform_data msm_otg_pdata = {
 	.phy_init_seq		= vigor_phy_init_seq,
 	.mode			= USB_PERIPHERAL,
+	.mode      		= USB_OTG,
 	.otg_control		= OTG_PMIC_CONTROL,
 	.phy_type		= CI_45NM_INTEGRATED_PHY,
 	.vbus_power		= msm_hsusb_vbus_power,
@@ -7531,6 +7532,7 @@ static struct platform_device *vigor_devices[] __initdata = {
 #endif
 
 #if defined(CONFIG_USB_GADGET_MSM_72K) || defined(CONFIG_USB_EHCI_HCD)
+	&msm_device_hsusb_host,
 	&msm_device_otg,
 #endif
 #ifdef CONFIG_BATTERY_MSM
