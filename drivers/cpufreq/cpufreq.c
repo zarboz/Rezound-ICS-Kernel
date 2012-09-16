5c08214b26f1cd7a9d9daa4d6c1091c595b3b4a3
vinylfreak89 <debruin.aaron@gmail.com>
2012-09-15 20:57:22 -0400

    Cleaned up CPU and VDD code.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -588,8 +588,10 @@ static ssize_t store_vdd_levels(struct cpufreq_policy *policy, const char *buf, 
 	int i = 0, j;
 	int pair[2] = { 0, 0 };
 	int sign = 0;
+
 	if (count < 1)
 		return 0;
+
 	if (buf[0] == '-') {
 		sign = -1;
 		i++;
@@ -598,8 +600,11 @@ static ssize_t store_vdd_levels(struct cpufreq_policy *policy, const char *buf, 
 		sign = 1;
 		i++;
 	}
+
 	for (j = 0; i < count; i++) {
+	
 		char c = buf[i];
+		
 		if ((c >= '0') && (c <= '9')) {
 			pair[j] *= 10;
 			pair[j] += (c - '0');
@@ -607,6 +612,7 @@ static ssize_t store_vdd_levels(struct cpufreq_policy *policy, const char *buf, 
 		else if ((c == ' ') || (c == '\t')) {
 			if (pair[j] != 0) {
 				j++;
+
 				if ((sign != 0) || (j > 1))
 					break;
 			}
@@ -614,6 +620,7 @@ static ssize_t store_vdd_levels(struct cpufreq_policy *policy, const char *buf, 
 		else
 			break;
 	}
+
 	if (sign != 0) {
 		if (pair[0] > 0)
 			acpuclk_set_vdd(0, sign * pair[0]);
